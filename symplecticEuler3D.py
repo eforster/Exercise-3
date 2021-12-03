08c8470fb25ff6ff085d935b673dd9b2e8989e3f
eforster <lizzieforster99@gmail.com>
2021-12-03 14:51:10 +0000

    code done ?!

--- symplecticEuler3D.py
+++ symplecticEuler3D.py
@@ -6,7 +6,10 @@ Produces plots of the position of the particle
 and its energy, both as function of time. Also
 saves both to file.
 
-The potential is U(r1, r2) = d_e * ((1 - exp(-alpha(r12 - r_e))) ** 2) - 1)
+The potential is given by:
+
+U(r1, r2) = d_e * ((1 - exp(-alpha(r12 - r_e))) ** 2) - 1)
+
 where the parameters are read in from a data file
 and passed to the functions that
 calculate force and potential energy.
@@ -24,7 +27,8 @@ def morse_force(particle, different_particle, r_e, d_e, alpha) :
     """
     Method to return the force on a particle
     in a double well potential using Morse Potential.
-    Force is given by
+    Force is given by:
+
     F(r1, r2) = 2 * alpha * d_e * (1 - exp(-alpha(r12 - r_e))) * exp(-alpha(r12 - r_e)) * r12_hat
 
     :param particle: Particle3D instance
@@ -45,7 +49,8 @@ def morse_force(particle, different_particle, r_e, d_e, alpha) :
 def morse_potential(particle, different_particle, r_e, d_e, alpha) :
     """
     Method to return Morse Potential
-    of particle in double-well potential using Morse Potential
+    of particle in double-well potential using Morse Potential:
+
     U(r1, r2) = d_e * ((1 - exp(-alpha(r12 - r_e))) ** 2) - 1)
 
     :param particle: Particle3D instance
@@ -64,7 +69,7 @@ def morse_potential(particle, different_particle, r_e, d_e, alpha) :
 def oscillations(pos_list, dt) :
     """
     Method to calculate the period of the oscillating particle
-    and then obtain the wave-number.
+    and then obtain the wave-number given by:
 
     wave-number = 1 / wavelength
 
@@ -72,35 +77,47 @@ def oscillations(pos_list, dt) :
     :param dt: time-step for simulation
 
     :return: wave_number
-
     """
+    wave_peaks = find_peaks(pos_list)                           # Uses scipy.signal find_peaks function to index all peaks
+    peak_pos = wave_peaks[0]                                    # Creates an array of the indices of all peaks
 
-    wave_peaks = find_peaks(pos_list)
-    print(wave_peaks[0])
-    peak_pos = [wave_peaks[0]]
+    first_peak = peak_pos[0]                                    # First peak
+    second_peak = peak_pos[1]                                   # Second peak
 
-    first_peak = peak_pos[0]
-    last_peak = peak_pos[1]
+    period = (len(pos_list[first_peak : second_peak])) * dt     # Calculates period
+    period_in_seconds = period * 1.018050571e-14                # Converts period to seconds
 
-    period = peak_pos[first_peak : last_peak]
-    period_in_seconds = period * 1.018050571e-14
-
-    frequency = 1 / period_in_seconds       # Hz
-    speed_of_light = 299792458              # m/s
-    wavelength = (speed_of_light / frequency) / 100         # converts to cm
+    frequency = 1 / period_in_seconds                           # Units : Hz
+    speed_of_light = 299792458                                  # Units : m/s
+    wavelength = (speed_of_light / frequency) * 100             # Units : m (multiply by 100 to convert to cm) -> cm
 
     wave_number = 1 / wavelength
-    return wave_number
-
+    return wave_number                                          # Units : cm^-1
 
 # Begin main code
 def main() :
+    """
+    The main method carries out the simulation in a few parts:
+
+    1.) Reads in data file from the command line
+    2.) Specifies initial conditions
+    3.) Initialises data lists for plotting later
+    4.) Starts a time integration loop
+    5.) Plots particle trajectory to screen
+    6.) Plots particle energy to screen
+    7.) Measures the frequency of oscillations and prints the wave-number to screen
+    8.) Measures the energy inaccuracy of the simulation and prints it to the screen
+
+    """
 
     with open(sys.argv[1], "r") as infile :
 
-        # Read name of output file from command line
+        # Part 1.) Reads in data file from the command line
+
+        # Read name of files from command line which needs 3 parts
         if len(sys.argv) != 3 :
 
+            # Helpful error message if the format is incorrect
             print("Wrong number of arguments.")
             print("Usage: " + sys.argv[0] + "<input file>" + "<output file>")
             quit()
@@ -110,80 +127,86 @@ def main() :
             # Open output file
             outfile = open(outfile_name, "w")
 
-            line1 = infile.readline()
-            line2 = infile.readline()
-            line2 = line2.split()
+            line1 = infile.readline()           # Processes line 1 of input file
+            line2 = infile.readline()           # Processes line 2 of input file
+            line2 = line2.split()               # Separates the parameters in line 2
 
+            # Helpful error message if there is not 2 parameters in line 2
             if len(line2) != 2 :
                 print("Wrong number of arguments in line 2 of input data file, i.e. simulation parameters. ")
 
             else :
-                dt = float(line2[0])
-                numstep = int(line2[1])
+                dt = float(line2[0])            # Reads in time-step for simulation
+                numstep = int(line2[1])         # Reads in number of steps for simulation
 
-            line3 = infile.readline()
-            line4 = infile.readline()
-            line4 = line4.split()
+            line3 = infile.readline()           # Processes line 3 of input file
+            line4 = infile.readline()           # Processes line 4 of input file
+            line4 = line4.split()               # Separates the parameters in line 4
 
+            # Helpful error message if there is not 3 parameters in line 4
             if len(line4) != 3 :
                 print("Wrong number of arguments in line 4 of input data file, i.e. Morse potential parameters. ")
 
             else :
-                d_e = float(line4[0])
-                r_e = float(line4[1])
-                alpha = float(line4[2])
+                d_e = float(line4[0])           # Reads in first Morse potential parameter
+                r_e = float(line4[1])           # Reads in second Morse potential parameter
+                alpha = float(line4[2])         # Reads in third Morse potential parameter
 
-            line5 = infile.readline()
-            line5 = line5.split()
-            line6 = infile.readline()
-            line6 = line6.split()
+            line5 = infile.readline()           # Processes line 5 of input file
+            line5 = line5.split()               # Separates the parameters in line 5
+            line6 = infile.readline()           # Processes line 6 of input file
+            line6 = line6.split()               # Separates the parameters in line 6
 
+
+            # Helpful error message if there is not 8 parameters in lines 5 and 6
             if len(line5) and len(line6) != 8 :
                 print("Wrong number of arguments in line 5 and 6, i.e. initial conditions of particles. ")
 
             else :
 
-                pos1 = np.array([float(line5[2]), float(line5[3]), float(line5[4])])
-                vel1 = np.array([float(line5[5]), float(line5[6]), float(line5[7])])
+                pos1 = np.array([float(line5[2]), float(line5[3]), float(line5[4])])    # Sets up Particle 1 position
+                vel1 = np.array([float(line5[5]), float(line5[6]), float(line5[7])])    # Sets up Particle 1 velocity
 
-                p1 = Particle3D(str(line5[0]), float(line5[1]), pos1, vel1)
+                p1 = Particle3D(str(line5[0]), float(line5[1]), pos1, vel1)             # Sets up Particle 1 as a particle3D instance
 
-                pos2 = np.array([float(line6[2]), float(line6[3]), float(line6[4])])
-                vel2 = np.array([float(line6[5]), float(line6[6]), float(line6[7])])
+                pos2 = np.array([float(line6[2]), float(line6[3]), float(line6[4])])    # Sets up Particle 2 position
+                vel2 = np.array([float(line6[5]), float(line6[6]), float(line6[7])])    # Sets up Particle 2 velocity
 
-                p2 = Particle3D(str(line6[0]), float(line6[1]), pos2, vel2)
+                p2 = Particle3D(str(line6[0]), float(line6[1]), pos2, vel2)             # Sets up Particle 2 as a particle3D instance
 
     infile.close()
 
+    # Part 2.) Specifies initial conditions
+
     time = 0.0
     p1_to_p2 = np.linalg.norm(p2.pos - p1.pos)
+    energy = p1.kinetic_e() + p2.kinetic_e() + morse_potential(p1, p2, r_e, d_e, alpha)     # Sums up initial energy total of system
+    outfile.write("{0:f} {1:f} {2:12.8f}\n".format(time, p1_to_p2, energy))                 # Formats output file being written
 
-    # Write out initial conditions
-    energy = p1.kinetic_e() + p2.kinetic_e() + morse_potential(p1, p2, r_e, d_e, alpha)
-    outfile.write("{0:f} {1:f} {2:12.8f}\n".format(time, p1_to_p2, energy))
+    # Part 3.) Initialises data lists for plotting later
 
-    # Initialise data lists for plotting later
     time_list = [time]
     pos1_list = [p1.pos]
     pos2_list = [p2.pos]
-    pos_list = [np.linalg.norm(p2.pos - p1.pos)]
+    pos_list = [np.linalg.norm(p2.pos - p1.pos)]            # Position list is | r2 - r1 | from particle positions
     energy_list = [energy]
 
-    # Start the time integration loop
+    # Part 4.) Starts a time integration loop
+
     for i in range(numstep) :
+
         # Update particle position
         p1.update_pos(dt)
         p2.update_pos(dt)
         p1_to_p2 = np.linalg.norm(p2.pos - p1.pos)
 
-        # Calculate force
+        # Calculate Morse force
         force1 = morse_force(p1, p2, r_e, d_e, alpha)
         force2 = - force1
 
         # Update particle velocity 
         p1.update_vel(dt, force1)
         p2.update_vel(dt, force2)
-
 
         # Increase time
         time += dt
@@ -203,22 +226,38 @@ def main() :
     # Close output file
     outfile.close()
 
-    # Plot particle trajectory to screen
+    # Part 5.) Plots particle trajectory to screen
+
     pyplot.title('Symplectic Euler : Position vs Time')
-    pyplot.xlabel('Time : ')
-    pyplot.ylabel('Position : ')
+    pyplot.xlabel('Time (* 1.018050571e-14 s) : ')
+    pyplot.ylabel('Position (Angstroms) : ')
     pyplot.plot(time_list, pos1_list)
     pyplot.plot(time_list, pos2_list)
     pyplot.show()
 
-    # Plot particle energy to screen
+    # Part 6.) Plots particle energy to screen
+
     pyplot.title('Symplectic Euler : Total Energy vs Time')
-    pyplot.xlabel('Time : ')
-    pyplot.ylabel('Energy : ')
+    pyplot.xlabel('Time (* 1.018050571e-14 s) : ')
+    pyplot.ylabel('Energy (eV) : ')
     pyplot.plot(time_list, energy_list)
     pyplot.show()
 
-    print("Wavenumber : ", oscillations(pos_list, dt), "cm ^ -1 .")
+    # Part 7.) Measures the frequency of oscillations and prints the wave-number to screen
+
+    print("Wave-number :", oscillations(pos_list, dt), "cm ^ -1 .")
+
+    # Part 8.) Measures the energy inaccuracy of the simulation and prints it to the screen
+
+    initial_energy = p1.kinetic_e() + p2.kinetic_e() + morse_potential(p1, p2, r_e, d_e, alpha)
+    max_energy = max(energy_list)
+    min_energy = min(energy_list)
+
+    delta_energy = max_energy - min_energy
+    inaccuracy = delta_energy / initial_energy
+
+    print("Energy inaccuracy : +/- ", inaccuracy, "eV ")
+
 
 # Execute main method, but only when directly invoked
 if __name__ == "__main__" :
